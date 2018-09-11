27d392d28bea231aa0e555679c89f26da4d416f7
Ryan Brady <ryanbrady369@gmail.com>
2018-09-11 22:16:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if (position_taken?(board, index + 1) && index < 10)
+  if (position_taken?(board, index) && index < 10)
     return true
   end
   false
